4cc31d44d4e682c1cac142ccc8167f0ab9be17c7
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-04-05 16:39:37 +0200

    lochalstorage added

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -11,6 +11,17 @@ export const DataProvider = (props) => {
     ])
     const [todoName, setTodoName] = useState("");
 
+    // localstorage' dan kayıtları okuyoruz...
+    useEffect(() => {
+        const todoStore = JSON.parse(localStorage.getItem("todoStore"))
+        if (todoStore) setTodos(todoStore)
+    }, [])
+
+    // localstorage' e kayıt yapıyoruz
+    useEffect(() => {
+        localStorage.setItem("todoStore", JSON.stringify(todos))
+    }, [todos])
+
     return (
         <DataContext.Provider value={[todos, setTodos, todoName, setTodoName]} >
             {props.children}
